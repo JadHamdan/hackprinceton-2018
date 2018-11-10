d179094864881d5d137dee208e355abf5b7ecb47
Marcel Goh <marcel.goh@mail.mcgill.ca>
2018-11-10 01:42:39 -0500

    login page and chat layout added

---
+++ coursetalk/build/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "marcelgoh.github.io/coursetalk/precache-manifest.d37777ddb2174f70d2a00ec56929b6eb.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("marcelgoh.github.io/coursetalk/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
--- coursetalk/server.js
+++ coursetalk/server.js
@@ -1,12 +1,40 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const Chatkit = require('@pusher/chatkit-server')
 
 const app = express()
+
+const chatkit = new Chatkit.default({
+    instanceLocator: 'v1:us1:3b31d86f-e2f8-4671-92e2-3a0e2d460298',
+    key: '57202e98-fea7-41e0-8720-7ecf6aefeb74:Hi1uucW/XBI8xzwVF3LaRL+cttw+KC6wu7PZoXyv6qg=',
+})
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
+
+app.post('/users', (req, res) => {
+    const { username } = req.body
+    chatkit
+        .createUser({
+            id: username,
+            name: username
+        })
+        .then(() => res.sendStatus(201))
+        .catch(error => {
+            if (error.error_type === 'services/chatkit/user_already_exists') {
+                res.sendStatus(200)
+            } else {
+                res.status(error.status).json(error)
+            }
+        })
+})
+
+app.post('/authenticate', (req, res) => {
+    const authData = chatkit.authenticate({userId: req.query.user_id })
+    res.status(authData.status).send(authData.body)
+})
 
 const PORT = 3001
 app.listen(PORT, err => {
